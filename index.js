3f74abd17a7eaae70bf388a27ba327fb452aac1c
KimCuong236 <23kimcuong@gmail.com>
2025-07-29 13:41:21 +0700

    update middleware

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import cors from "cors";
 import connectDB from "./src/common/configs/db.js";
 import { HOST, PORT } from "./src/common/configs/enviroments.js";
 import setupSwagger from "./src/common/configs/swagger-config.js";
+import jsonValidator from "./src/common/middleware/json.middleware.js";
+import notFoundHandler from "./src/common/middleware/notFound.middleware.js";
+import errorHandler from "./src/common/middleware/error.middleware.js";
 connectDB();
 const app = express();
 app.use(
@@ -17,6 +20,15 @@ app.use(express.json());
 
 app.use("/api", router);
 setupSwagger(app);
+// Middleware xử lý JSON không hợp lệ
+app.use(jsonValidator);
+
+// Middleware xử lý route không tồn tại
+app.use(notFoundHandler);
+
+// Middleware xử lý lỗi chung
+app.use(errorHandler);
+
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
   console.log(`Swagger Docs available at http://${HOST}:${PORT}/api-docs`);
